8b9382781e65016a3bf011a690236120bb44bb32
jfgiraud <jfgiraud@gmail.com>
2013-03-21 12:39:47 +0100

    exception

--- src/temmentalr/RpnStack.java
+++ src/temmentalr/RpnStack.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import sun.org.mozilla.javascript.Interpreter;
 import temmental2.Node;
 
 public class RpnStack extends Stack {
---
+++ src/temmentalr/StackException.java
@@ -0,0 +1,9 @@
+package temmentalr;
+
+public class StackException extends RuntimeException {
+
+	public StackException(String message) {
+		super(message);
+	}
+	
+}
