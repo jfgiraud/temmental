317e456cca98af0fa012290b30caea131ecc5753
Jean-François Giraud <jeanfrancois.giraud@orange.com>
2016-08-28 09:12:30 +0200

    Add transform functions

--- src/main/java/com/github/jfgiraud/temmental/Transforms.java
+++ src/main/java/com/github/jfgiraud/temmental/Transforms.java
@@ -1,6 +1,7 @@
 package com.github.jfgiraud.temmental;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 import static com.github.jfgiraud.temmental.TemplateUtils.getDeclaredMethod;
 
@@ -33,9 +34,99 @@ public class Transforms {
         }
     };
 
-    static final Transform<Boolean,Boolean> NOT = new Transform<Boolean, Boolean>() {
+    static final ParamTransform<Object[], Object, Boolean> NOT_EQUALS = new ParamTransform<Object[], Object, Boolean>() {
+        public Boolean apply(Object[] values, Object value) {
+            return value != null ? !value.equals(values[0]) : (value != values[0]);
+        }
+    };
+
+    static final ParamTransform<Comparable[], Comparable, Boolean> LESS_THAN = new ParamTransform<Comparable[], Comparable, Boolean>() {
+        public Boolean apply(Comparable[] values, Comparable value) {
+            return value.compareTo(values[0]) < 0;
+        }
+    };
+
+    static final ParamTransform<Comparable[], Comparable, Boolean> LESS_EQUALS = new ParamTransform<Comparable[], Comparable, Boolean>() {
+        public Boolean apply(Comparable[] values, Comparable value) {
+            return value.compareTo(values[0]) <= 0;
+        }
+    };
+
+    static final ParamTransform<Comparable[], Comparable, Boolean> GREATER_THAN = new ParamTransform<Comparable[], Comparable, Boolean>() {
+        public Boolean apply(Comparable[] values, Comparable value) {
+            return value.compareTo(values[0]) > 0;
+        }
+    };
+
+    static final ParamTransform<Comparable[], Comparable, Boolean> GREATER_EQUALS = new ParamTransform<Comparable[], Comparable, Boolean>() {
+        public Boolean apply(Comparable[] values, Comparable value) {
+            return value.compareTo(values[0]) >= 0;
+        }
+    };
+
+    static final Transform<Boolean, Boolean> NOT = new Transform<Boolean, Boolean>() {
         public Boolean apply(Boolean value) {
             return !value;
+        }
+    };
+
+    static final ParamTransform<Boolean[], Boolean, Boolean> AND = new ParamTransform<Boolean[], Boolean, Boolean>() {
+        public Boolean apply(Boolean[] values, Boolean value) {
+            if (!value) return false;
+            for (Boolean b : values) {
+                if (!b) return false;
+            }
+            return true;
+        }
+    };
+
+    static final ParamTransform<Boolean[], Boolean, Boolean> OR = new ParamTransform<Boolean[], Boolean, Boolean>() {
+        public Boolean apply(Boolean[] values, Boolean value) {
+            if (value) return true;
+            for (Boolean b : values) {
+                if (b) return true;
+            }
+            return false;
+        }
+    };
+
+
+    static final Transform<Collection, Boolean> EMPTY = new Transform<Collection, Boolean>() {
+        public Boolean apply(Collection value) {
+            return value.isEmpty();
+        }
+    };
+
+    static final Transform<Collection, Integer> SIZE = new Transform<Collection, Integer>() {
+        public Integer apply(Collection value) {
+            return value.size();
+        }
+    };
+
+    static final Transform<Collection<Boolean>, Boolean> ALL = new Transform<Collection<Boolean>, Boolean>() {
+        public Boolean apply(Collection<Boolean> values) {
+            for (Boolean b : values) {
+                if (!b) return false;
+            }
+            return true;
+        }
+    };
+
+    static final Transform<Collection<Boolean>, Boolean> ANY = new Transform<Collection<Boolean>, Boolean>() {
+        public Boolean apply(Collection<Boolean> values) {
+            for (Boolean b : values) {
+                if (b) return true;
+            }
+            return false;
+        }
+    };
+
+    static final Transform<Collection<Boolean>, Boolean> NONE = new Transform<Collection<Boolean>, Boolean>() {
+        public Boolean apply(Collection<Boolean> values) {
+            for (Boolean b : values) {
+                if (b) return false;
+            }
+            return true;
         }
     };
 
--- src/test/java/com/github/jfgiraud/temmental/TransformTest.java
+++ src/test/java/com/github/jfgiraud/temmental/TransformTest.java
@@ -6,10 +6,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import static com.github.jfgiraud.temmental.TemplateUtils.createModel;
 import static org.junit.Assert.assertEquals;
@@ -24,12 +21,25 @@ public class TransformTest {
     @Before
     public void setUp() throws Exception {
         transforms = new HashMap();
+        //TODO README
         transforms.put("if", Transforms.IF);
         transforms.put("ifn", Transforms.IF_NOT);
         transforms.put("not", Transforms.NOT);
         transforms.put("upper", Transforms.UPPER);
         transforms.put("lower", Transforms.LOWER);
         transforms.put("eq", Transforms.EQUALS);
+        transforms.put("ne", Transforms.NOT_EQUALS);
+        transforms.put("lt", Transforms.LESS_THAN);
+        transforms.put("le", Transforms.LESS_EQUALS);
+        transforms.put("gt", Transforms.GREATER_THAN);
+        transforms.put("ge", Transforms.GREATER_EQUALS);
+        transforms.put("and", Transforms.AND);
+        transforms.put("or", Transforms.OR);
+        transforms.put("empty", Transforms.EMPTY);
+        transforms.put("size", Transforms.SIZE);
+        transforms.put("all", Transforms.ALL);
+        transforms.put("any", Transforms.ANY);
+        transforms.put("none", Transforms.NONE);
         properties = new Properties();
         TemplateRecorder.setRecording(true);
         out = new StringWriter();
@@ -69,6 +79,24 @@ public class TransformTest {
     }
 
     @Test
+    public void testAnd() throws IOException {
+        StringTemplate template = new StringTemplate("before~$a:'and<$b>#true~OK~#true~after", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", true, "b", true);
+        assertEquals("beforeOKafter", template.format(model));
+        model = createModel("a", true, "b", false);
+        assertEquals("beforeafter", template.format(model));
+    }
+
+    @Test
+    public void testOr() throws IOException {
+        StringTemplate template = new StringTemplate("before~$a:'or<$b>#true~OK~#true~after", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", true, "b", true);
+        assertEquals("beforeOKafter", template.format(model));
+        model = createModel("a", false, "b", true);
+        assertEquals("beforeOKafter", template.format(model));
+    }
+
+    @Test
     public void testIfNotTwoArgs() throws IOException {
         StringTemplate template = new StringTemplate("before~$bool:'ifn<\"##false-block##\",\"##true-block##\">:'upper~after", transforms, properties, Locale.ENGLISH);
         model = createModel("bool", true);
@@ -93,6 +121,96 @@ public class TransformTest {
         assertEquals("OK", template.format(model));
         model = createModel("a", 1, "b", "1");
         assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testLessThan() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'lt<$b>#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", 4, "b", 5);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", 5, "b", 5);
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testLessEquals() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'le<$b>#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", 4, "b", 5);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", 5, "b", 5);
+        assertEquals("OK", template.format(model));
+    }
+
+    @Test
+    public void testGreaterThan() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'gt<$b>#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", 5, "b", 4);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", 5, "b", 5);
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testGreaterEquals() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'ge<$b>#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", 5, "b", 4);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", 5, "b", 5);
+        assertEquals("OK", template.format(model));
+    }
+
+    @Test
+    public void testNotEquals() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'ne<$b>#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", 4, "b", 5);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", 4, "b", 4);
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testEmpty() throws IOException {
+        StringTemplate template = new StringTemplate("~$a:'empty#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", Arrays.asList());
+        assertEquals("OK", template.format(model));
+        model = createModel("a", Arrays.asList(1));
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testAll() throws IOException {
+        StringTemplate template = new StringTemplate("~($a,$b):'all#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", true, "b", true);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", true, "b", false);
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testAll2() throws IOException {
+        StringTemplate template = new StringTemplate("~$l:'all#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("l", Arrays.asList(true, true));
+        assertEquals("OK", template.format(model));
+        model = createModel("l", Arrays.asList(true, false));
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testAny() throws IOException {
+        StringTemplate template = new StringTemplate("~($a,$b):'any#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", false, "b", true);
+        assertEquals("OK", template.format(model));
+        model = createModel("a", false, "b", false);
+        assertEquals("", template.format(model));
+    }
+
+    @Test
+    public void testNone() throws IOException {
+        StringTemplate template = new StringTemplate("~($a,$b):'none#true~OK~#true~", transforms, properties, Locale.ENGLISH);
+        model = createModel("a", false, "b", true);
+        assertEquals("", template.format(model));
+        model = createModel("a", false, "b", false);
+        assertEquals("OK", template.format(model));
     }
 
 }
